04487cf7063d4b2e95e070b67b633110d7287913
paul.miller@medtronhex.com <paul.miller@medtronhex.com>
2023-09-24 16:44:04 -0400

    homework 2 create-react-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello CSCI E-39!</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Paul Miller
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
